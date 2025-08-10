52a5d21884be155c72a1bba655b9f4d9796f6010
sureshdeveloperofficial <sureshdeveloperofficial@gmail.com>
2025-08-10 05:20:24 -0400

    logo added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@
 const NavBar = () => {
   return (
     <nav className="fixed top-0 left-0 z-50 md:p-9 p-3">
-      <img src="/images/nav-logo.svg" alt="nav-logo" className="md:w-24 w-20 cursor-hover" />
+      <img src="/images/suresh_logo-transaparent.png" alt="sovrayne-logo" className="md:w-30 w-25 cursor-hover" />
     </nav>
   );
 };
